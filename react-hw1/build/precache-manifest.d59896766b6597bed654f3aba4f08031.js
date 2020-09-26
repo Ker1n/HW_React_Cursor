a858ac938526d8591135ee8c36ca86ae5c585e93
Markov Kyrylo <Ker1n.mkc@gmail.com>
2020-09-26 11:39:25 +0300

    HW1

---
+++ react-hw1/build/precache-manifest.d59896766b6597bed654f3aba4f08031.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f7e88fe0a37945b61998132863777a",
+    "url": "/HW_React_Cursor/react-hw1/index.html"
+  },
+  {
+    "revision": "c9809c5969397bb7b143",
+    "url": "/HW_React_Cursor/react-hw1/static/css/main.bd3bfdbd.chunk.css"
+  },
+  {
+    "revision": "8fe9dbdcaa8375588c9c",
+    "url": "/HW_React_Cursor/react-hw1/static/js/2.86586acc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW_React_Cursor/react-hw1/static/js/2.86586acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9809c5969397bb7b143",
+    "url": "/HW_React_Cursor/react-hw1/static/js/main.e36ce3c9.chunk.js"
+  },
+  {
+    "revision": "51ac50f04de47b2e89ab",
+    "url": "/HW_React_Cursor/react-hw1/static/js/runtime-main.79082aa1.js"
+  },
+  {
+    "revision": "e9b8a89205aaaeca86a6ec142bd1af8c",
+    "url": "/HW_React_Cursor/react-hw1/static/media/rey.e9b8a892.jpg"
+  },
+  {
+    "revision": "5fe3ceb5784004cd21679dc3d0e2d597",
+    "url": "/HW_React_Cursor/react-hw1/static/media/skywalker.5fe3ceb5.jpg"
+  }
+]);
