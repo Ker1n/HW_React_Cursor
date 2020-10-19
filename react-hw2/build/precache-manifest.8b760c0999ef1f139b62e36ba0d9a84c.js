3d904bb1a31d4ff0713446f5d7348be35ac38d8d
Markov Kyrylo <Ker1n.mkc@gmail.com>
2020-10-19 22:04:48 +0300

    react hw#2

---
+++ react-hw2/build/precache-manifest.8b760c0999ef1f139b62e36ba0d9a84c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "432a94eea13c07a975c3e4c4014dede5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3cf448faa0b257c95e0",
+    "url": "/static/css/main.b3c7cb5f.chunk.css"
+  },
+  {
+    "revision": "4a15fb24d12e2621dc7c",
+    "url": "/static/js/2.7f7167fb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.7f7167fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3cf448faa0b257c95e0",
+    "url": "/static/js/main.ad5beaea.chunk.js"
+  },
+  {
+    "revision": "a89d63612fd19cecad42",
+    "url": "/static/js/runtime-main.a768652b.js"
+  },
+  {
+    "revision": "9d6b4e9fcb6e1cec1087516662b1a85a",
+    "url": "/static/media/robo.9d6b4e9f.png"
+  }
+]);
