f3401b18b53e67f8e728c4964ce5bc934ea53d0b
Markov Kyrylo <Ker1n.mkc@gmail.com>
2020-10-21 22:30:40 +0300

    react hw#3

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 https://ker1n.github.io/HW_React_Cursor/react-hw3/build/index.html
+
 https://ker1n.github.io/HW_React_Cursor/react-hw2/build/index.html
+
 https://ker1n.github.io/HW_React_Cursor/react-hw1/build/index.html
---
+++ react-hw3/build/precache-manifest.77fbbd92a2d5af73027c5ca79e40aff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b33fb59d9f505495bf057f2e5b9190",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73855dddfdc264ac419e",
+    "url": "/static/css/main.d25aa1ed.chunk.css"
+  },
+  {
+    "revision": "84ec096d6c9d47eedee4",
+    "url": "/static/js/2.bd3285f2.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.bd3285f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73855dddfdc264ac419e",
+    "url": "/static/js/main.13ae83ef.chunk.js"
+  },
+  {
+    "revision": "9986b8644d7db6178d57",
+    "url": "/static/js/runtime-main.02980717.js"
+  }
+]);
