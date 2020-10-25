c0c65f8b55704797cae749390d78fc8d7e956a74
Markov Kyrylo <Ker1n.mkc@gmail.com>
2020-10-25 23:20:08 +0300

    test

--- react-hw4/src/components/SuccessPage/SuccessPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SuccessImage from './succes.jpg';
-import './SuccessPage.css'
-
-const SuccessPage = () => { 
-    return <div className = 'success-Image'><img src={SuccessImage} alt="SuccessPage"/></div>
-}
-
-export default SuccessPage
